eb52764e9322ed34e5bed8991312f50a5bbfbf92
Werberth Vinicius <werberthvinicius@gmail.com>
2018-01-01 17:18:20 -0300

    Adding Product Model

--- shop/models.py
+++ shop/models.py
@@ -19,3 +19,38 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        related_name='products'
+    )
+    name = models.CharField(
+        max_length=200,
+        db_index=True
+    )
+    slug = models.SlugField(
+        max_length=200,
+        db_index=True
+    )
+    image = models.ImageField(
+        upload_to='products/%Y/%m/%d',
+        blank=True
+    )
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
