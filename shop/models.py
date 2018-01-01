f8c39268baf10d8be6e7790d8ebcbd6aa445be57
Werberth Vinicius <werberthvinicius@gmail.com>
2018-01-01 17:04:03 -0300

    Adding model Category

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        db_index=True
+    )
+    slug = models.SlugField(
+        max_length=200,
+        db_index=True,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
