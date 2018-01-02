e391f2190559fabf7d102aeb52d37b61f8a12122
Werberth Vinicius <werberthvinicius@gmail.com>
2018-01-02 03:39:38 -0300

    Fixing imports bugs

--- cart/forms.py
+++ cart/forms.py
@@ -12,5 +12,5 @@ class CartAddProductForm(forms.Form):
     update = forms.BooleanField(
         required=False,
         initial=False,
-        widget=forms.HiddenIput
+        widget=forms.HiddenInput
     )
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
-from .models import CartAddProductForm
+from .forms import CartAddProductForm
 
 
 @require_POST
