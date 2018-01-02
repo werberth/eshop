bc996f69b4d2e86cec208615816d443544748dc3
Werberth Vinicius <werberthvinicius@gmail.com>
2018-01-02 04:00:36 -0300

    Adding button to insert product on cart

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -27,6 +26,10 @@ def product_detail(request, id, slug):
         slug=slug,
         available=True
     )
+    cart_product_form = CartAddProductForm()
     template = 'shop/product/detail.html'
-    context = {'product': product}
+    context = {
+        'product': product,
+        'cart_product_form': cart_product_form
+    }
     return render(request, template, context)
