39d12328fce28c13737220eb6455482230320838
Werberth Vinicius <werberthvinicius@gmail.com>
2018-01-03 02:24:39 -0300

    Adding OrderItem model

--- orders/models.py
+++ orders/models.py
@@ -21,3 +21,19 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return 'Order {}'.format(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items')
+    product = models.ForeignKey(
+        Product,
+        related_name='order_items'
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
