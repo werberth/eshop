f4ad314c89589b06740e36e54e909f0256ebd171
Werberth Vinicius <werberthvinicius@gmail.com>
2018-01-05 22:56:42 -0300

    Adding order_create view

--- eshop/urls.py
+++ eshop/urls.py
@@ -6,8 +6,10 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^orders/', include('orders.urls', namespace='orders')),
     url(r'^', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
